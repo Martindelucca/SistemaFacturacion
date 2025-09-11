8e0e2544a1e83344704c5099ead0e5aa7a033af1
Martin de lucca <martindelucca3@hotmail.com>
2025-09-11 18:38:35 -0300

    Agregue interfaces de servicios

---
+++ Servicios/Interfaces/IFormaPagoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01.Servicios.Interfaces
+{
+    internal interface IFormaPagoService
+    {
+    }
+}
